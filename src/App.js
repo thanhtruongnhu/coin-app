ab6f45cc69fec9b7e714df2602d0ebca850a16ac
truong.nhu <35893692+thanhtruongnhu@users.noreply.github.com>
2021-03-20 12:27:40 -0300

    insert page transition animation (framer-motion)

--- src/App.js
+++ src/App.js
@@ -1,41 +1,79 @@
 import { Flex } from '@chakra-ui/layout';
 // import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
 import Header from './Header';
-
+import { AnimatePresence, motion } from 'framer-motion';
 import PortFolio from './PortFolio';
 import Sidebar from './Sidebar';
 
 function App() {
+	const location = useLocation();
+
+	const pageVariants = {
+		in: {
+			opacity: 1,
+			// scale: 1,
+		},
+		out: {
+			opacity: 0,
+			// scale: 1.05,
+		},
+	};
+
+	const pageTransition = {
+		duration: 0.25,
+	};
+
 	return (
 		<div className="App">
-			<Router>
-				<Header />
-				{/* Box: d="flex" */}
-				<Flex>
-					<Sidebar />
-
-					<Switch>
-					<Route path="/TopCrypto">
-							TOP CRYPTO
+			<Header />
+			{/* Box: d="flex" */}
+			<Flex>
+				<Sidebar />
+				<AnimatePresence exitBeforeEnter>
+					<Switch location={location} key={location.pathname}>
+						<Route path="/TopCrypto">
+							<motion.div
+								initial="out"
+								animate="in"
+								exit="out"
+								variants={pageVariants}
+								transition={pageTransition}
+							>
+								TOP CRYPTO
+							</motion.div>
 						</Route>
 						<Route path="/Chart">
-							CHART
+							<motion.div
+								initial="out"
+								animate="in"
+								exit="out"
+								variants={pageVariants}
+								transition={pageTransition}
+							>
+								CHART
+							</motion.div>
 						</Route>
-						<Route exact path="/PortFolio">
-							<PortFolio />
+						<Route path="/PortFolio">
+							<motion.div
+								initial="out"
+								animate="in"
+								exit="out"
+								variants={pageVariants}
+								transition={{ duration: 0.65}}
+							>
+								<PortFolio />
+							</motion.div>
 						</Route>
 						<Route exact path="/">
 							<PortFolio />
 						</Route>
 					</Switch>
-				</Flex>
+				</AnimatePresence>
+			</Flex>
 
-				{/* Switch: LoginScreen */}
-				{/* Switch: Top Stock */}
-				{/* Switch: Chart */}
-				{/* Switch: Chat Channels */}
-			</Router>
+			{/* Switch: LoginScreen */}
+			{/* Switch: Chat Channels */}
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import theme from './theme';
 import { Provider } from 'react-redux';
 import store from './app/store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
 	<>
 		<ChakraProvider>
 			<Provider store={store}>
 				<ColorModeScript initialColorMode={theme.config.initialColorMode} />
-				<App />
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
 			</Provider>
 		</ChakraProvider>
 	</>,
